234e4f17354957a4d0a6849c24b82cbcdf6b66f0
John Li <lijohn@gmail.com>
2024-05-24 21:25:01 +0800

    Add project files.

---
+++ User/Default.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FanHub.User
+{
+    public partial class Default : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Index.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                Control sliderControl = (Control)Page.LoadControl("sliderController.ascx");
+                Panel.Controls.Add(sliderControl);
+            }
+        }
+    }
+}
